282d7b85fea4a1223f20fa0dfe66c9cf03c611d6
Wilson Santos Designer <wilsonsantosdesigner@gmail.com>
2026-05-08 21:19:04 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/developer/StartApp.java
@@ -0,0 +1,13 @@
+package com.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+
+}
---
+++ src/main/java/com/developer/models/ContasReceber.java
@@ -0,0 +1,39 @@
+package com.developer.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_contas_receber")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+public class ContasReceber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String descricao;
+
+    @Column(name = "valor_receber")
+    Double valorReceber;
+
+    @Column(name = "data_vencimento")
+    Date dataVencimento;
+
+    @Column(name = "data_recebimento")
+    Date dataRecebimento;
+
+    @Column(name = "valor_recebido")
+    Double valorRecebido;
+
+
+
+
+
+}
